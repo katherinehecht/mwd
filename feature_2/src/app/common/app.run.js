e765b7c64e1bb78af023f587b8a7f56b751d45c5
dbozzo@nd.edu <dominicbozzo1@gmail.com>
2020-03-05 16:40:12 -0500

    parse config stuff added in feature 2

--- feature_2/src/app/common/app.run.js
+++ feature_2/src/app/common/app.run.js
@@ -1,6 +1,12 @@
 function init($uiRouter){
     var Visualizer = window['ui-router-visualizer'].Visualizer;
     var pluginInstance = $uiRouter.plugin(Visualizer);
+
+    Parse.serverURL = 'https://parseapi.back4app.com'; // This is your Server URL
+    Parse.initialize(
+    'BCrUQVkk80pCdeImSXoKXL5ZCtyyEZwbN7mAb11f', // This is your Application ID
+    '4wPYRKbpTJeCdmFNaS31AiQZ8344aaYubk6Uo8VW' // This is your Javascript key
+    );
 }
 
 angular
