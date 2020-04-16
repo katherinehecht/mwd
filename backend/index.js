46e4ababd63117bda7bd7dab7e5f6e0b38f56027
katherinehecht <31599030+katherinehecht@users.noreply.github.com>
2020-04-16 13:45:55 -0700

    Working on index.js

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,9 @@
 //import nodeFoursquare from 'node-foursquare';
 const express = require('express');
-const config = {
-  'secrets' : {
-    'clientId' : 'KUQDWHVZC5SVE5HWPUWKPXD0FXDCX4W1V10BIYUQ3HJF40DB',
-    'clientSecret' : 'MMHS1CW43PKDCVDCN2R3WXBQC20N54MDFPOH2YFN5SPMOTGC',
-    'redirectUrl' : 'https://bender.com'
-  }
-}
-
+var foursquare = require('node-foursquare-venues')('KUQDWHVZC5SVE5HWPUWKPXD0FXDCX4W1V10BIYUQ3HJF40DB', 'MMHS1CW43PKDCVDCN2R3WXBQC20N54MDFPOH2YFN5SPMOTGC');
+var callback;// response
 //const Foursquare = nodeFoursquare(config);
+
 const app = express();
 const httpServer = require('http')
   .createServer(app);
@@ -19,29 +14,7 @@ httpServer.listen(port, function(){
   console.log("API running on port" + port + '.');
 });
 
-
-/*
-app.get('/', (req: express$Request, res: express$Response) => {
-  const url = Foursquare.getAuthClientRedirectUrl();
-  res.writeHead(303, { location: url });
-  res.end();
+app.get('/callback', (req, res) => {
+  foursquare.venues.categories(callback)
+    res.send(callback);
 });
-
-app.get('/callback', (req: express$Request, res: express$Response) => {
-  const code = ((req.query.code: any): string);
-
-  Foursquare.getAccessToken(
-    {
-      code,
-    },
-    (error: ?Error, accessToken: ?string) => {
-      if (error) {
-        res.send(`An error was thrown: ${error.message}`);
-      } else if (!accessToken) {
-        res.send(`No access token was provided`);
-      } else {
-        // Save access token and continue.
-      }
-    }
-  );
-});*/
