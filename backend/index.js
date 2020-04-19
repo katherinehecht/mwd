8c0627061df038fb9c95518dc685dc5afeac4f58
Andy Shin <andyshin2@gmail.com>
2020-04-19 14:05:37 -0700

    foursquare api & express server

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 //import nodeFoursquare from 'node-foursquare';
 const express = require('express');
-var foursquare = require('node-foursquare-venues')('KUQDWHVZC5SVE5HWPUWKPXD0FXDCX4W1V10BIYUQ3HJF40DB', 'MMHS1CW43PKDCVDCN2R3WXBQC20N54MDFPOH2YFN5SPMOTGC');
+const fetch = require('node-fetch');
+//var foursquare = require('node-foursquare-venues')('KUQDWHVZC5SVE5HWPUWKPXD0FXDCX4W1V10BIYUQ3HJF40DB', 'MMHS1CW43PKDCVDCN2R3WXBQC20N54MDFPOH2YFN5SPMOTGC');
 var callback;// response
 //const Foursquare = nodeFoursquare(config);
 
@@ -15,6 +16,12 @@ httpServer.listen(port, function(){
 });
 
 app.get('/callback', (req, res) => {
-  foursquare.venues.categories(callback)
-    res.send(callback);
+  const url = 'https://api.foursquare.com/v2/venues/categories?client_secret=MMHS1CW43PKDCVDCN2R3WXBQC20N54MDFPOH2YFN5SPMOTGC&client_id=KUQDWHVZC5SVE5HWPUWKPXD0FXDCX4W1V10BIYUQ3HJF40DB&v=20200419';
+  fetch(url)
+    .then(
+        response => response.text() // .json(), etc.
+        // same as function(response) {return response.text();}
+    ).then(
+        html => res.send(html)
+    );
 });
